d4a24ea445edc795cd98c3e01c861603eea1e33b
Siim Raudsepp <siimraudsepp@msn.com>
2025-06-15 02:59:15 +0300

    No requirement of Overhaullib

--- COGetTransformMatrixFix/COGetTransformMatrixFixModSystem.cs
+++ COGetTransformMatrixFix/COGetTransformMatrixFixModSystem.cs
@@ -10,11 +10,8 @@ namespace COGetTransformMatrixFix;
 
 public class COGetTransformMatrixFixModSystem : ModSystem
 {
-    private static ICoreClientAPI _clientApi;
-
-    public override void StartClientSide(ICoreClientAPI api)
+    public override void AssetsFinalize(ICoreAPI api)
     {
-        _clientApi = api;
         var harmony = new Harmony(Mod.Info.ModID);
 
         var original =
@@ -24,35 +21,6 @@ public class COGetTransformMatrixFixModSystem : ModSystem
 
         harmony.Patch(original, prefix: new HarmonyMethod(patch));
 
-        // var original =
-        //     AccessTools.Method(typeof(ShapeElementCollider), "GetTransformMatrix",
-        //         new[] { typeof(int), typeof(float[]) });
-        // var patch = AccessTools.Method(typeof(COGetTransformMatrixFixModSystem), nameof(Patch));
-        //
-        // harmony.Patch(original, finalizer: new HarmonyMethod(patch));
-    }
-
-    [HarmonyFinalizer]
-    [HarmonyPatch(typeof(ShapeElementCollider), "GetTransformMatrix")]
-    public static Exception Patch(Exception __exception, int jointId, float[] TransformationMatrices4x4)
-    {
-        if (__exception == null)
-        {
-            return null;
-        }
-
-        try
-        {
-            _clientApi.SendChatMessage("Exception while getting transform matrix: " + __exception.Message,
-                GlobalConstants.InfoLogChatGroup);
-        }
-        catch (Exception e)
-        {
-            // ignored
-        }
-
-
-        return null;
     }
 
     [HarmonyPrefix]
